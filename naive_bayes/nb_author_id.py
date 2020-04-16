176b4918370630be56acaa627ba9da58806e5745
lucianacristea <63721421+lucianacristea@users.noreply.github.com>
2020-04-16 15:16:32 +0200

    Update nb_author_id.py

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -13,7 +13,7 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-from class_vis import prettyPicture, output_image
+###from class_vis import prettyPicture, output_image
 
 
 ### features_train and features_test are the features for the training
@@ -29,9 +29,33 @@ t0 = time()
 clf.fit(features_train, labels_train)
 print ("Time to train:", round(time()-t0, 3), "s")
 
+###new plot
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Plot the decision boundary. For that, we will assign a color to each
+# point in the mesh [x_min, x_max]x[y_min, y_max].
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+plt.pcolormesh(xx, yy, Z, cmap=plt.cm.Paired)
+
+# Plot also the training points
+plt.scatter(X[:, 0], X[:, 1], c=Y, cmap=plt.cm.Paired, edgecolors='k')
+plt.title('3-Class classification using Naive Bayes algorithm')
+plt.axis('tight')
+plt.show()
+
+###########
+
+
 ### draw the decision boundary with the text points overlaid
-prettyPicture(clf, features_test, labels_test)
-output_image("test.png", "png", open("test.png", "rb").read())
+###prettyPicture(clf, features_test, labels_test)
+###output_image("test.png", "png", open("test.png", "rb").read())
 
 t0 = time()
 pred=clf.predict(features_test)
