e6e89e5920070b2d70dbc874c3e09932248c69bf
lucianacristea <63721421+lucianacristea@users.noreply.github.com>
2020-04-16 15:45:28 +0200

    Update nb_author_id.py

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -36,8 +36,9 @@ import matplotlib.pyplot as plt
 # Plot the decision boundary. For that, we will assign a color to each
 # point in the mesh [x_min, x_max]x[y_min, y_max].
 h = .01  # step size in the mesh
-x_min, x_max = features_train[:, 0].min() - 1, features_train[:, 0].max() + 1
-y_min, y_max = features_train[:, 1].min() - 1, features_train[:, 1].max() + 1
+X = features_train.data[:, :2]
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
 xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
 Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
 
