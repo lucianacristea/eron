dc54e12c1e61f6c77d3166e20f21ad623ade87c8
lucianacristea <63721421+lucianacristea@users.noreply.github.com>
2020-04-18 19:54:20 +0200

    Update svm_author_id.py

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -22,6 +22,8 @@ from sklearn.svm import SVC
 features_train, features_test, labels_train, labels_test = preprocess()
 clf = SVC(kernel='linear')
 t0 = time()
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
 clf.fit(features_train, labels_train)
 print ("Time to train:", round(time()-t0, 3), "s")
 
