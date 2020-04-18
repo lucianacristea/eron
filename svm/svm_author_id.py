313260f82c4a2de734d1407cfd3e0becd8f1efdb
lucianacristea <63721421+lucianacristea@users.noreply.github.com>
2020-04-18 16:07:25 +0200

    Update svm_author_id.py

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -16,12 +16,26 @@ from email_preprocess import preprocess
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
+
+from sklearn.svm import SVC
 features_train, features_test, labels_train, labels_test = preprocess()
+clf = SVC(gamma='auto')
+t0 = time()
+clf.fit(features_train, labels_train)
+print ("Time to train:", round(time()-t0, 3), "s")
 
+t0 = time()
+pred=clf.predict(features_test)
+print ("Time to make prediction:", round(time()-t0, 3), "s")
 
+### calculate and return the accuracy on the test data
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(pred, labels_test)
+print("Accuracy of Naive Bayes predictor is: {}".format(accuracy))
+##################################################################
 
-
-#########################################################
-### your code goes here ###
+### draw the decision boundary with the text points overlaid
+prettyPicture(clf, features_test, labels_test)
+output_image("test.png", "png", open("test.png", "rb").read())
 
 #########################################################
