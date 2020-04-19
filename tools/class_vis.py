b8473f8b31e4f90ddf035e7f896309914529a10e
lucianacristea <63721421+lucianacristea@users.noreply.github.com>
2020-04-19 16:33:00 +0200

    Update class_vis.py

--- tools/class_vis.py
+++ tools/class_vis.py
@@ -54,7 +54,7 @@ def prettyPicture(clf, X_test, y_test):
 
 import base64
 ###import json
-import simplejson as json
+import json as simplejson
 import subprocess
     
 def output_image(name, format, bytes):
