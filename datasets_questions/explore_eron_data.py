2cb5f393b69ca3a259520fcc3524469a0fd5cf99
lucianacristea <63721421+lucianacristea@users.noreply.github.com>
2020-04-23 17:53:45 +0200

    Update explore_eron_data.py

--- datasets_questions/explore_eron_data.py
+++ datasets_questions/explore_eron_data.py
@@ -15,5 +15,5 @@ import pickle
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
 print("People (data points) in dataset: {}".format(len(enron_data)))
 for key, value in enron_data.items():
-    print("No. of attributes for one person (data point) in dataset: {}".format(len(filter(bool, value))))
+    print("No. of attributes for one person (data point) in dataset: {}".format(len(list(filter(bool, value)))))
     break
