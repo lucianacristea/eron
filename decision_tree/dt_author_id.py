9c9d5474e83f0e8391e88ccd371eaa62636994e2
lucianacristea <63721421+lucianacristea@users.noreply.github.com>
2020-04-23 16:14:58 +0200

    Update dt_author_id.py

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -18,43 +18,30 @@ from class_vis import prettyPicture, output_image
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 
-clf = SVC(kernel='rbf', C=10000.0)
+clf = DecisionTreeClassifier(min_samples_split=40)
 t0 = time()
-features_train = features_train[:int(len(features_train)/100)]
-labels_train = labels_train[:int(len(labels_train)/100)]
+### features_train = features_train[:int(len(features_train)/100)]
+### labels_train = labels_train[:int(len(labels_train)/100)]
 clf.fit(features_train, labels_train)
 print("Time to train:", round(time()-t0, 3), "s")
 
 t0 = time()
 pred=clf.predict(features_test)
-### print("Element 9:", pred[10])
-### print("Element 10:", pred[10])
-### print("Element 26:", pred[26])
-### print("Element 50:", pred[50])
-sara=0
-chris=0
-for pr in pred:
-    if pr==0:
-        sara+=1
-    else:
-        chris+=1
-print("Sara: ", sara)
-print("Chris: ", chris)
 print("Time to make prediction:", round(time()-t0, 3), "s")
 
 ### calculate and return the accuracy on the test data
 from sklearn.metrics import accuracy_score
-accuracy = accuracy_score(pred, labels_test)
-print("Accuracy of SVM predictor is: {}".format(accuracy))
+accuracy = accuracy_score(labels_test, pred)
+print("Accuracy of Decision Tree predictor is: {}".format(accuracy))
 ##################################################################
 
 ### draw the decision boundary with the text points overlaid
 ### we only take the first two features.
 new_features_train=features_train[:, :2]
 new_features_test=features_test[:, :2]
-new_clf= SVC(kernel='rbf', C=10000.0)
+new_clf= DecisionTreeClassifier(min_samples_split=40)
 new_clf.fit(new_features_train, labels_train)
 import matplotlib.pyplot as plt
 plt=prettyPicture(new_clf, new_features_test, labels_test)
