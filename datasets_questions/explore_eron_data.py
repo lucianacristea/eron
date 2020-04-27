a4bf68f19381fb02c820763bd1bd2112cb621e3f
lucianacristea <63721421+lucianacristea@users.noreply.github.com>
2020-04-27 21:19:04 +0200

    Update explore_eron_data.py

--- datasets_questions/explore_eron_data.py
+++ datasets_questions/explore_eron_data.py
@@ -26,6 +26,9 @@ for key, value in enron_data.items():
         print("Total value of the stock belonging to {} is {}".format(key, value["total_stock_value"]))
     if key=="COLWELL WESLEY":
         print("Number of email messages from {} to POI is {}".format(key, value["from_this_person_to_poi"]))
+    if key=="SKILLING JEFFREY K":
+        print("Value of stock options exercised by {} is {}".format(key, value["exercised_stock_options"]))
+ 
  
 print("In the data set there are {} POI (persons of interest).".format(k))
 
