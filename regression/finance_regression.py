68406ab170162794d22e7c9abdd1271f196916c3
lucianacristea <63721421+lucianacristea@users.noreply.github.com>
2020-05-03 19:55:19 +0200

    Update finance_regression.py

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -39,4 +39,28 @@ clf=linear_model.LinearRegression()
 reg= clf.fit(feature_train, target_train)
 print("Slope: {}".format(reg.coef_))
 print("Intercept: {}".format(reg.intercept_))
-print("Score: {}".format(reg.score(feature_train, target_train))
+print("Score: {}".format(reg.score(feature_train, target_train)))
+
+
+### draw the scatterplot, with color-coded training and testing points
+#import matplotlib.pyplot as plt
+#for feature, target in zip(feature_test, target_test):
+#    plt.scatter( feature, target, color=test_color ) 
+#for feature, target in zip(feature_train, target_train):
+#    plt.scatter( feature, target, color=train_color ) 
+
+### labels for the legend
+#plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
+#plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
+
+
+### draw the regression line, once it's coded
+#try:
+#    plt.plot( feature_test, reg.predict(feature_test) )
+#except NameError:
+#    pass
+#plt.xlabel(features_list[1])
+#plt.ylabel(features_list[0])
+#plt.legend()
+#plt.show()
+
