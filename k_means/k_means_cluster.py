620af018d2fcd85fa1db5b316d8851a243cbcad7
lucianacristea <63721421+lucianacristea@users.noreply.github.com>
2020-05-12 20:23:17 +0200

    Update k_means_cluster.py

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -46,7 +46,11 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
+
+""" 
+###cluster with 2 features
 features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
@@ -59,11 +63,26 @@ poi, finance_features = targetFeatureSplit( data )
 for f1, f2 in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
+"""
+###cluster with 3 features
+features_list = [poi, feature_1, feature_2, feature_3]
+data = featureFormat(data_dict, features_list )
+poi, finance_features = targetFeatureSplit( data )
+
+
+### in the "clustering with 3 features" part of the mini-project,
+### you'll want to change this line to 
+### for f1, f2, _ in finance_features:
+### (as it's currently written, the line below assumes 2 features)
+for f1, f2, _ in finance_features:
+    plt.scatter( f1, f2 )
+plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
 from sklearn.cluster import KMeans
-kmeans = KMeans(n_clusters=2).fit(finance_features)
+###kmeans = KMeans(n_clusters=2).fit(finance_features)
+kmeans = KMeans(n_clusters=3).fit(finance_features)
 pred=kmeans.predict(finance_features)
 
 
@@ -73,3 +92,5 @@ try:
     Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print("no predictions object named pred found, no clusters to plot")
+    
+ 
