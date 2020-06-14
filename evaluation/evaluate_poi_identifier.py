985f438160e8315946f5e30a465b6099f979b6cd
lucianacristea <63721421+lucianacristea@users.noreply.github.com>
2020-06-14 15:30:53 +0200

    Update evaluate_poi_identifier.py

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -20,6 +20,22 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list, sort_keys = '../tools/python2_lesson14_keys.pkl')
 labels, features = targetFeatureSplit(data)
 
-
-
 ### your code goes here 
+from sklearn.model_selection import train_test_split
+###features_train, features_test, labels_train, labels_test = train_test_split(features, labels)
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size = 0.3, random_state = 42)
+
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier()
+
+clf = clf.fit(features_train, labels_train)
+###clf = clf.fit(features, labels)
+pred=clf.predict(features_test)
+###pred=clf.predict(features)
+
+### calculate and return the accuracy on the test data
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test, pred)
+###accuracy = accuracy_score(labels, pred)
+print("Accuracy of Decision Tree predictor is: {}".format(accuracy))
+##################################################################
