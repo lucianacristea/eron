df05949c50c6e13c505c8141f16fbe058a8bb607
lucianacristea <63721421+lucianacristea@users.noreply.github.com>
2020-06-14 15:52:28 +0200

    Update evaluate_poi_identifier.py

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -33,8 +33,10 @@ clf = clf.fit(features_train, labels_train)
 pred=clf.predict(features_test)
 ###pred=clf.predict(features)
 import numpy as np
+from sklearn.metrics import confusion_matrix, precision_score, recall_score, classification_report
 print ("Number of POI's: ", np.count_nonzero(pred))
 print ("People in Test Set: ", len(pred)) 
+print confusion_matrix(labels_test, pred)
 
 ### calculate and return the accuracy on the test data
 from sklearn.metrics import accuracy_score
